d6d7fec90a6fd4d5e8ca65a0b7b51229c3b72bae
Dhruv Vegad <dhruvavegad@gmail.com>
2025-11-11 14:48:25 +0530

    terraform aws nginx project finished

--- terraform-aws-nginx/main.tf
+++ terraform-aws-nginx/main.tf
@@ -12,11 +12,11 @@ provider "aws" {
 }
 
 
-# Renamed to reflect its new purpose
+
 resource "aws_security_group" "allow_http_ssh" {
   name_prefix = "nginx_sec_grp"
 
-  # Rule for HTTP (Port 80)
+
   ingress {
     from_port   = 80
     to_port     = 80
@@ -24,7 +24,7 @@ resource "aws_security_group" "allow_http_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # ⬇️ ADDED: Rule for SSH (Port 22)
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -44,7 +44,7 @@ resource "aws_instance" "ec2-instance" {
   ami           = "ami-02b8269d5e85954ef"
   instance_type = "t2.micro"
 
-  # ⬇️ UPDATED: Use the new security group name
+
   vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
   
   subnet_id = "subnet-01a456c8f9df7db15"
@@ -65,7 +65,7 @@ resource "aws_instance" "ec2-instance" {
   EOF
 }
 
-# This will print the IP to make it easy to connect
+
 output "public_ip" {
   value = aws_instance.ec2-instance.public_ip
 }
